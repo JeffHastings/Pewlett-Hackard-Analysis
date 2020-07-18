861723a1a0a375561125dbf70f762f9f6c8dd29b
Jeff Hastings <66492485+JeffHastings@users.noreply.github.com>
2020-07-17 19:04:16 -0500

    Updated through Module 7.3.1

--- schema.sql
+++ schema.sql
@@ -47,4 +47,69 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no)
 );
 
-SELECT*FROM departments;
+SELECT*FROM dept_manager;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+-- Creating tables for PH-EmployeeDB-Challenge
+
+CREATE TABLE Retiring_Employees (
+	 emp_no INT NOT NULL,
+	 first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE Mentorship_Eligibility (
+	 emp_no INT NOT NULL,
+	 first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
